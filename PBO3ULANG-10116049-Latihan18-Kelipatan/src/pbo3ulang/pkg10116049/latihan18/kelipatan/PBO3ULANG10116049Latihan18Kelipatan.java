6e74178adfdc4c97677c82635513f67703b91d0a
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-02-18 16:42:25 +0700

    Add files via upload

---
+++ PBO3ULANG-10116049-Latihan18-Kelipatan/src/pbo3ulang/pkg10116049/latihan18/kelipatan/PBO3ULANG10116049Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan18.kelipatan;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA              : M. Ibad Guthwara
+ * KELAS             : PBO 3 ULANG
+ * NIM               : 10116049
+ * Deskripsi program : Program ini berisi untuk menampilkan
+ */
+public class PBO3ULANG10116049Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i;      
+        for (i=1; i<=10; i+=1){
+            System.out.println(3.5*i);
+        }
+    }
+    
+}
